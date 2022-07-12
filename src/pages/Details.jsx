801d86f9539a92ab7b3744ca61c5ccf27e7a0938
Michael2000-kpi <85711932+Michael2000-kpi@users.noreply.github.com>
2022-07-13 01:44:48 +0300

    Add files via upload

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { IoArrowBack } from 'react-icons/io5';
+import { searchByCountry} from '../config';
+import { Button } from '../components/Button';
+import { Info } from '../components/Info';
+import {ALL_COUNTRIES} from '../config';
 export const Details = () => {
   const { name } = useParams();
+
+  const navigate = useNavigate();
+  const [countries,setCountries]=useState(null);
+  
+  useEffect(() => {
+    axios.get(searchByCountry(name)).then(({ data})=>setCountries(data[0]));
+
+  },[name])
+  
   return (
     <div>
-      Details {name}
+    
+      <Button onClick={() => navigate(-1)}><IoArrowBack />Back</Button>
+      {countries && <Info {...countries} />}
+     
     </div>
   );
 };

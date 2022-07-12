4a9467b69ba1ce2dcb30b0e835db590ae46db26e
Michael2000-kpi <85711932+Michael2000-kpi@users.noreply.github.com>
2022-07-13 01:44:14 +0300

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 const BASE_URL = 'https://restcountries.com/v2/';
-export const ALL_COUNTRIES=BASE_URL+"all?fields=name,flags,population,capital,region";
-export const searchByContry=(name)=>BASE_URL+'name/'+name;
-export const filterByCode=(code)=>BASE_URL+'alpha?code'+code.join('');
+
+export const ALL_COUNTRIES = BASE_URL + 'all?fields=name,capital,flags,population,region';
+
+export const searchByCountry = (name) => BASE_URL + 'name/' + name;
+
+export const filterByCode = (codes) => BASE_URL + 'alpha?codes=' + codes.join(',');
